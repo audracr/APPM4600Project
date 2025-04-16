28d0081a89ae31db4a9249a441953c63579f586a
BriceGillespie <chulitobandito@gmail.com>
2025-04-16 11:27:36 -0600

    Update Rosenbrock_Lazy_Newton.py

--- Rosenbrock_Lazy_Newton.py
+++ Rosenbrock_Lazy_Newton.py
@@ -1,154 +1,161 @@
 import numpy as np
-import numpy.linalg as la
-from scipy.linalg import inv
+from scipy.linalg import solve  
 from matplotlib import pyplot as plt
-from scipy import differentiate
 
-
-def Rosen_fun_d1(x_guess):
-    term_1 = 100*(x_guess[1]-x_guess[0]**2)**2+(x_guess[0]-1)**2
-    return term_1
-
-def grad(x_guess):
-    df_dx0 = 200*(x_guess[1]-x_guess[0]**2)*(-2*x_guess[0])+2*(x_guess[0]-1)
-    df_dx1 = 200*(x_guess[1]-x_guess[0]**2)
-    return np.array([df_dx0, df_dx1])
-
-def Hessian(x_guess):
-    H = differentiate.hessian(Rosen_fun_d1,x_guess)
+# d1 version
+def hessian_fd(fun, x, h=1e-5):
+    x = np.asarray(x, dtype=float)
+    n = x.size
+    H = np.zeros((n, n))
+    for i in range(n):
+        e_i = np.zeros(n)
+        e_i[i] = 1.0
+        # diagonal second derivative using central difference
+        H[i, i] = (fun(x + h*e_i) - 2*fun(x) + fun(x - h*e_i)) / (h**2)
+        for j in range(i+1, n):
+            e_j = np.zeros(n)
+            e_j[j] = 1.0
+            # partial derivatives using central difference
+            H[i, j] = (fun(x + h*e_i + h*e_j) - fun(x + h*e_i - h*e_j) 
+                        - fun(x - h*e_i + h*e_j) + fun(x - h*e_i - h*e_j)) / (4*h**2)
+            H[j, i] = H[i, j]
     return H
 
-def Lazy_Newton_Rosenbrack_d1(tol):
-    count = 0 # this variable is a safegaurd against infinite loops
-    x = np.array([15,15]) # this is our initial guess, and later our x_n
-    rn = np.array([x])
-    x1 = 2 * x # after this, we use x1 to save our x_n-1
-    p_k = x # in general p_k = x_k+1 - x_k
-    while (count < 10000 and (np.abs(Rosen_fun_d1(x)-Rosen_fun_d1(x1))) > tol):
-        #While we're done less then 100 iters #While |f(x_n)-f(x_n-1)| is less then tol
-        count = count + 1
-        x1 = x # save x_n-1
+def Rosen_fun_d1(x):
+    return 100.0 * (x[1] - x[0]**2)**2 + (x[0] - 1.0)**2
 
-        g_k = grad(x)
-        if (count != 0): # if we should update the hessian
-            hessian = Hessian(x).ddf
-            B_k = hessian
-         # for Newton direction
-        print(g_k)
-        print(-g_k)
-        p_k = (inv(B_k))@(g_k)
-        alpha = 10
-        for i in range(1,51):
-            alpha = alpha / i
-            x = x - alpha*p_k
-            print(Rosen_fun_d1(x))
-            print(Rosen_fun_d1(x1))
-            if (Rosen_fun_d1(x)<Rosen_fun_d1(x1)):
-                break
-            else:
-                x = x1
-        #print('x is:')
-        #print(x)
-        #print(Rosen_fun_d1(x))
-        rn = np.append(rn, np.array([x]), axis=0)
-        #print('rn')
-        #print(rn)
-        #break
-    print('num iters: ')
-    print(count)
-    print('xmin: ')
-    print(x)
-    print(rn)
+def grad(x):
+    dfdx0 = -400 * x[0] * (x[1] - x[0]**2) + 2*(x[0] - 1.0)
+    dfdx1 = 200 * (x[1] - x[0]**2)
+    return np.array([dfdx0, dfdx1])
+
+def Lazy_Newton_Rosenbrock_d1(tol=1e-12, max_iter=10000):
+    count = 0
+    # diff initial guess
+    x = np.array([1.2, 1.2])
+    iterates = [x.copy()]
     
-    rN=x
-    rnN = rn
+    while count < max_iter:
+        g = grad(x)
+        grad_norm = np.linalg.norm(g)
+        if grad_norm < tol:
+            break
+        
+        # compute Hessian using finite differences
+        H = hessian_fd(Rosen_fun_d1, x)
+
+        p = solve(H, g)
+        
+        # kine search logic
+        alpha = 1.0
+        x_new = x - alpha * p
+        ls_iter = 0
+        max_ls_iter = 10  
+        
+        while Rosen_fun_d1(x_new) >= Rosen_fun_d1(x) and ls_iter < max_ls_iter:
+            alpha *= 0.5
+            x_new = x - alpha * p
+            ls_iter += 1
+        
+        if Rosen_fun_d1(x_new) >= Rosen_fun_d1(x):
+            print("Line search failed")
+            break
+        
+        x = x_new.copy()
+        iterates.append(x.copy())
+        count += 1
+        
+        print(f"Iter: {count:4d} | x = {x} | f(x) = {Rosen_fun_d1(x):.6e} | ||grad|| = {grad_norm:.3e}")
     
-    numN = rnN.shape[0];
-    print(rnN[0:(numN-1)])
-    errN = np.max(np.abs(rnN[0:(numN-1)]-rN),1);
-    plt.plot(np.arange(numN-1),np.log10(errN+1e-18),'b-o',label='Newton');
-    plt.title('Newton iteration log10|r-rn|');
-    plt.legend();
-    plt.show();
+    print("\nFinal x (d1):", x)
+    print("Number of iterations (d1):", count)
+    
+    iterates = np.array(iterates)
+    final_x = x
+    errors = np.linalg.norm(iterates - final_x, axis=1)
+    
+    plt.figure(figsize=(8,5))
+    plt.plot(np.arange(len(errors)), np.log10(errors + 1e-18), 'b-o', label='Newton finite-diff')
+    plt.title('Newton Iteration: log₁₀|x - x*| (Finite Differences)')
+    plt.xlabel('Iteration')
+    plt.ylabel('log₁₀(Error)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
-Lazy_Newton_Rosenbrack_d1(1e-16)
+# d2 
 
-#print(Rosen_fun_d1(np.array([guess,guess])))
-#print(Hessian(np.array([guess,guess])))
+def Rosen_fun_d2(x):
+    return 100.0 * (x[1] - x[0]**2)**2 + (x[0] - 1.0)**2
 
+def grad_d2(x):
+    #analytic gradient for the Rosenbrock function
+    dfdx = -400 * x[0] * (x[1] - x[0]**2) + 2 * (x[0] - 1.0)
+    dfdy = 200 * (x[1] - x[0]**2)
+    return np.array([dfdx, dfdy])
 
-def Rosen_fun_d2(x_guess):
-    term_1 = 10*(x_guess[1]-x_guess[0]**2)**2+(x_guess[0]-1)**2
-    term_2 = 10*(x_guess[2]-x_guess[1]**2)**2+(x_guess[1]-1)**2
-    return term_1 + term_2
-
-def grad_d2(x_guess):
-    df_dx0 = 20*(x_guess[1]-x_guess[0]**2)*(-2*x_guess[0])+2*(x_guess[0]-1)
-    df_dx1 = 20*(x_guess[1]-x_guess[0]**2)+20*(x_guess[2]-x_guess[1]**2)*(-2*x_guess[1])+2*(x_guess[1]-1)
-    df_dx2 = 20*(x_guess[2]-x_guess[1]**2)
-    return np.array([df_dx0, df_dx1, df_dx2])
-
-def Hessian_d2(x_guess):
-    H = differentiate.hessian(Rosen_fun_d2,x_guess)
+def hessian_analytic_d2(x):
+    # analytic hessian for the rosenbrock function:
+    H = np.zeros((2,2))
+    H[0,0] = 1200 * x[0]**2 - 400 * x[1] + 2
+    H[0,1] = -400 * x[0]
+    H[1,0] = -400 * x[0]
+    H[1,1] = 200
     return H
 
-def Lazy_Newton_Rosenbrack_d2(tol):
-    count = 0 # this variable is a safegaurd against infinite loops
-    x = np.array([1.2,1.2,1.2]) # this is our initial guess, and later our x_n
-    rn = np.array([x])
-    x1 = 2 * x # after this, we use x1 to save our x_n-1
-    p_k = x # in general p_k = x_k+1 - x_k
-    while (count < 100 and (np.abs(Rosen_fun_d2(x)-Rosen_fun_d2(x1))) > tol):
-        #While we're done less then 100 iters #While |f(x_n)-f(x_n-1)| is less then tol
-        count = count + 1
-        x1 = x # save x_n-1
-
-        g_k = grad_d2(x)
-        if (count != 0): # if we should update the hessian
-            hessian = Hessian_d2(x).ddf
-            B_k = hessian
-         # for Newton direction
-        print(g_k)
-
-        p_k = (inv(B_k))@(g_k)
-        print((inv(B_k)))
-        alpha = 10
-        for i in range(1,101):
-            alpha = alpha / i
-            x = x - alpha*p_k
-            print(Rosen_fun_d2(x))
-            print(Rosen_fun_d2(x1))
-            if (Rosen_fun_d2(x)<Rosen_fun_d2(x1)):
-                break
-            else:
-                x = x1
-        #print('x is:')
-        #print(x)
-        #print(Rosen_fun_d1(x))
-        rn = np.append(rn, np.array([x]), axis=0)
-        #print('rn')
-        #print(rn)
-        #break
-    print('num iters: ')
-    print(count)
-    print('xmin: ')
-    print(x)
-    print(rn)
+def Lazy_Newton_Rosenbrock_d2(tol=1e-12, max_iter=10000):
+    count = 0
+    # same initial guess
+    x = np.array([1.2, 1.2])
+    iterates = [x.copy()]
     
-    rN=x
-    rnN = rn
+    while count < max_iter:
+        g = grad_d2(x)
+        grad_norm = np.linalg.norm(g)
+        if grad_norm < tol:
+            break
+
+        H = hessian_analytic_d2(x)
+        
+        p = solve(H, g)
+        alpha = 1.0
+        x_new = x - alpha * p
+        ls_iter = 0
+        max_ls_iter = 10
+        while Rosen_fun_d2(x_new) >= Rosen_fun_d2(x) and ls_iter < max_ls_iter:
+            alpha *= 0.5
+            x_new = x - alpha * p
+            ls_iter += 1
+        
+        if Rosen_fun_d2(x_new) >= Rosen_fun_d2(x):
+            print("Line search failed")
+            break
+        
+        x = x_new.copy()
+        iterates.append(x.copy())
+        count += 1
+        
+        print(f"Iter: {count:4d} | x = {x} | f(x) = {Rosen_fun_d2(x):.6e} | ||grad|| = {grad_norm:.3e}")
     
-    numN = rnN.shape[0];
-    print(rnN[0:(numN-1)])
-    errN = np.max(np.abs(rnN[0:(numN-1)]-rN),1);
-    plt.plot(np.arange(numN-1),np.log10(errN+1e-18),'b-o',label='Newton');
-    plt.title('Newton iteration log10|r-rn|');
-    plt.legend();
-    plt.show();
+    print("\nFinal x (d2):", x)
+    print("Number of iterations (d2):", count)
+    iterates = np.array(iterates)
+    final_x = x
+    errors = np.linalg.norm(iterates - final_x, axis=1)
+    
+    plt.figure(figsize=(8,5))
+    plt.plot(np.arange(len(errors)), np.log10(errors + 1e-18), 'r-o', label='Newton analytic')
+    plt.title('Newton Iteration: log₁₀|x - x*| (Analytic Hessian)')
+    plt.xlabel('Iteration')
+    plt.ylabel('log₁₀(Error)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
-Lazy_Newton_Rosenbrack_d2(1e-16)
+print("Running d1 version with finite differences for Hessian:")
+Lazy_Newton_Rosenbrock_d1()
 
-#print(Rosen_fun_d1(np.array([guess,guess])))
-#print(Hessian(np.array([guess,guess])))
+print("\nRunning d2 version with analytic Hessian:")
+Lazy_Newton_Rosenbrock_d2()
 
 
